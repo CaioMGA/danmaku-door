7342951bbc9ed05d290b88bac4ff192502524f58
Caio M.G.A <caiomga@gmail.com>
2024-02-13 16:17:13 -0300

    add Globals; add fitness dialogue

---
+++ scripts/Globals.gd
@@ -0,0 +1,4 @@
+extends Node
+
+@export var bullet_rank = 0
+@export var enemy_rank = 0
--- scripts/tests/dialogue-testing.gd
+++ scripts/tests/dialogue-testing.gd
@@ -1,1 +1,6 @@
 extends Node2D
+
+var diag = load("res://dialogues/fitness-gal.dialogue")
+
+func _ready():
+	DialogueManager.show_dialogue_balloon(diag, "start")
